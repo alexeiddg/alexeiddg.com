c97aa6203e936c2e026b15001a4afbbf8a4d2caf
alexeiddg <a01637405@tec.mx>
2024-05-29 19:46:39 -0600

    Feat: Added Button to Hero Section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider";
 import "../styles/globals.css";
-import {ThemeProvider} from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
